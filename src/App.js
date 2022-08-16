1789444c58f39828eb8bc07f7fa7ccbcdd57e60a
keltsjsx <kelts.jsx@gmail.com>
2022-08-16 23:09:54 +0700

    Basic React State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: [],
+    };
+  }
+
+  getUser = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users/1"
+    );
+    const { data } = response;
+    if (data) {
+      this.setState({
+        user: data,
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.getUser();
+  }
+
   render() {
+    const { username } = this.state.user;
     return (
       <>
-        <h1>Hello World</h1>
+        <h1>Hello, my name is {username}</h1>
       </>
     );
   }
